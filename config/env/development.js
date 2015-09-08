c4fc528c7a441d6b9bce45a69a67a01c938c761e
unknown <rbelavad@rbelavad.in.ibm.com>
2015-09-08 22:23:31 +0530

    pushing my code

---
+++ config/env/development.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+ // db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME,
+  
+  debug: true,
+  logging: {
+    format: 'tiny'
+  },
+  //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
+  aggregate: false,
+  mongoose: {
+    debug: false
+  },
+ // hostname: 'http://localhost:3000',
+  hostname: process.env.OPENSHIFT_NODEJS_IP +':'+ process.env.OPENSHIFT_NODEJS_PORT ,
+  app: {
+    name: 'MEAN - A Modern Stack - Development'
+  },
+  strategies: {
+    local: {
+      enabled: true
+    },
+    landingPage: '/',
+    facebook: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
+      enabled: false
+    },
+    twitter: {
+      clientID: 'DEFAULT_CONSUMER_KEY',
+      clientSecret: 'CONSUMER_SECRET',
+      callbackURL: 'http://localhost:3000/api/auth/twitter/callback',
+      enabled: false
+    },
+    github: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/api/auth/github/callback',
+      enabled: false
+    },
+    google: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/api/auth/google/callback',
+      enabled: false
+    },
+    linkedin: {
+      clientID: 'DEFAULT_API_KEY',
+      clientSecret: 'SECRET_KEY',
+      callbackURL: 'http://localhost:3000/api/auth/linkedin/callback',
+      enabled: false
+    }
+  },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER', // Gmail, SMTP
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
+    }
+  }, 
+  secret: 'SOME_TOKEN_SECRET'
+};
